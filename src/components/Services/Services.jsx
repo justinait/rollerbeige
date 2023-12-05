0e7bcf0aa051b9593df84ae6387b563c364ca631
justinait <justiiturraspe@gmail.com>
2023-12-05 15:30:35 -0300

    services

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -3,8 +3,6 @@ import './Services.css'
 import HandshakeOutlinedIcon from '@mui/icons-material/HandshakeOutlined';
 import HandymanOutlinedIcon from '@mui/icons-material/HandymanOutlined';
 import PaletteOutlinedIcon from '@mui/icons-material/PaletteOutlined';
-import AutoStoriesOutlinedIcon from '@mui/icons-material/AutoStoriesOutlined';
-import ChatOutlinedIcon from '@mui/icons-material/ChatOutlined';
 import EngineeringOutlinedIcon from '@mui/icons-material/EngineeringOutlined';
 
 function Services() {
