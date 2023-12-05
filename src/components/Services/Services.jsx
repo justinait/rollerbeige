0f9a2aa0d57eea26c4230264a01c5bc963db28c1
justinait <justiiturraspe@gmail.com>
2023-12-05 13:06:05 -0300

    services

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Services.css'
 import HandshakeOutlinedIcon from '@mui/icons-material/HandshakeOutlined';
 import HandymanOutlinedIcon from '@mui/icons-material/HandymanOutlined';
 import PaletteOutlinedIcon from '@mui/icons-material/PaletteOutlined';
@@ -8,18 +9,26 @@ import EngineeringOutlinedIcon from '@mui/icons-material/EngineeringOutlined';
 
 function Services() {
   return (
-    <div>
-      <h2>SERVICIOS</h2>
-      <PaletteOutlinedIcon/>
-      Asesoramiento personalizado.
-      <EngineeringOutlinedIcon/>
-      Instalaciones
-      <HandshakeOutlinedIcon />
-      Servicio Postventa
-      <ChatOutlinedIcon/>
-
-      <HandymanOutlinedIcon />
-      Reparaciones
+    <div className='services'>
+      <h3>SERVICIOS</h3>
+      <div className='servicesContainer'>
+        <div className=''>
+          <PaletteOutlinedIcon/>
+          Asesoramiento personalizado.
+        </div>
+        <div>
+          <EngineeringOutlinedIcon/>
+          Instalaciones
+        </div>
+        <div>
+          <HandshakeOutlinedIcon />
+          Servicio Postventa
+        </div>
+        <div>
+          <HandymanOutlinedIcon />
+          Reparaciones
+        </div>
+      </div>
     </div>
   )
 }
