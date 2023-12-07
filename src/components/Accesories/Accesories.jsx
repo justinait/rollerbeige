ba78e1779e37bb0cd9d17828d9c108f58c9f0e95
justinait <justiiturraspe@gmail.com>
2023-12-07 11:46:54 -0300

    accessorios

--- src/components/Accesories/Accesories.jsx
+++ src/components/Accesories/Accesories.jsx
@@ -1,9 +1,92 @@
 import React from 'react'
 import './Accesories.css'
+import roller from '/images/curtains/cortinas (48).jpg'
+import bandas from '/images/curtains/cortinas (7).jpg'
+import tradicionales from '/images/curtains/hero.jpg'
 
 function Accesories() {
   return (
-    <div>Accesories</div>
+    <div>
+      <h2>ACCESORIOS</h2>
+
+      <h3>Para Roller</h3>
+      <div className='accesoriesContainer'>
+        <img src={roller} alt="Roller" className='accesoriesImage' />
+        <ul>
+          <li>
+            Mecanismos Roller (todas las medidas)
+          </li>
+          <li>
+          Cadenas Roller
+          </li>
+          <li>
+          Uniones y topes de cadenas de cadena 
+          </li>
+          <li>
+          Pesos de cadena
+          </li>
+          <li>
+          Zocalos de aluminio
+          </li>
+          <li>
+          Zocalos forrados
+          </li>
+        </ul>
+      
+      </div>
+      
+      <h3>Para Bandas verticales y Paneles orientales</h3>
+      <div className='accesoriesContainer'>
+        <img src={bandas} alt="Roller" className='accesoriesImage' />
+        <ul>
+          <li>
+          Cadenas de giro
+          </li>
+          <li>
+          Cadenas inferiores
+          </li>
+          <li>
+          Pesos de cadena
+          </li>
+          <li>
+          Bastones de aluminio
+          </li>
+        </ul>
+      </div>
+
+      <h3>Para Tradicionales</h3>
+
+      <div className='accesoriesContainer'>
+        <img src={tradicionales} alt="Roller" className='accesoriesImage' />
+        <ul>
+          <li>
+          Rieles europeos y super europeos (a bastón)
+          </li>
+          <li>
+          Barrales metálicos de 1” y ¾”, blancos, negros y níquel
+          </li>
+          <li>
+          Bastones de aluminio para rieles europeos
+          </li>
+          <li>
+          Ganchitos para cabezales de cortina
+          </li>
+          <li>
+          Patines para rieles
+          </li>
+          <li>
+          Cruces europeos y super europeos
+          </li>
+          <li>
+          Soportes de barrales
+          </li>
+          <li>
+          Ménsulas estándar y XL
+          </li>
+        </ul>     
+      </div>
+    </div>
+
   )
 }
 
--- src/components/Promos/Promos.jsx
+++ src/components/Promos/Promos.jsx
@@ -2,81 +2,15 @@ import React from 'react'
 import './Promos.css'
 import promo1 from '/images/curtains/cortinas (3).jpg'
 import promo2 from '/images/curtains/cortinas (2).jpg'
+import Accesories from '../Accesories/Accesories'
 
 function Promos() {
   return (
     <div>
-      <h2>ACCESORIOS</h2>
-        
-        <p>Accesorios ROLLER</p>
-        <ul>
-          <li>
-            Mecanismos Roller (todas las medidas) blancos, negros, beige y gris.    
-          </li>
-          <li>
-          Mecanismos Roller (todas las medidas) blancos, negros, beige y gris.
-          </li>
-          <li>
-          Cadenas Roller, blancas, negras, beige y gris.
-          </li>
-          <li>
-          Uniones y topes de cadenas de cadena 
-          </li>
-          <li>
-          Pesos de cadena, blancos, negros, beige y gris.
-          </li>
-          <li>
-          Zocalos de aluminio blancos, negros, beige y gris
-          </li>
-          <li>
-          Zocalos forrados
-          </li>
-        </ul>
-        
-        <p>Accesorios BANDAS VERTICALES y PANLES ORIENTALES</p>
-        <ul>
-          <li>
-          Cadenas de giro
-          </li>
-          <li>
-          Cadenas inferiores
-          </li>
-          <li>
-          Pesos de cadena
-          </li>
-          <li>
-          Bastones de aluminio
-          </li>
-        </ul>
-        <p>Accesorios TRADICIONALES</p>
-        <ul>
-          <li>
-          Rieles europeos y super europeos (a bastón)
-          </li>
-          <li>
-          Barrales metálicos de 1” y ¾”, blancos, negros y níquel
-          </li>
-          <li>
-          Bastones de aluminio para rieles europeos
-          </li>
-          <li>
-          Ganchitos para cabezales de cortina
-          </li>
-          <li>
-          Patines para rieles
-          </li>
-          <li>
-          Cruces europeos y super europeos
-          </li>
-          <li>
-          Soportes de barrales
-          </li>
-          <li>
-          Ménsulas estándar y XL
-          </li>
-        </ul>     
-        
-        
+      <br />
+      <br />
+      <Accesories/>
+        <h2>OUTLET</h2>
       <div className='promosDivContainer'>
         <div className='promoContainer'>
           <img src={promo1} alt="" className='promoImage' />
