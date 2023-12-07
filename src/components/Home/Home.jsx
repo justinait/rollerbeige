3af4903faacade2db665467cb9910ea05a1cead0
justinait <justiiturraspe@gmail.com>
2023-12-07 09:26:14 -0300

    hero

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Home.css'
-import hero from '/images/curtains/hero.jpg'
+// import hero from '/images/curtains/hero.jpg'
+import hero from '/images/curtains/cortinas (6).jpg'
 import Services from '../Services/Services'
 import ProductsHome from '../ProductsHome/ProductsHome'
 
