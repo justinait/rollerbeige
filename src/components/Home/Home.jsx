cc8437eedcff0cfe5282d4500b0a02e31a3a826b
justinait <justiiturraspe@gmail.com>
2023-12-20 12:30:05 -0300

    responsive

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './Home.css'
 import hero from '/images/hero4.png'
+import heroPc from '/images/curtains/heropc2.jpg'
 import Services from '../Services/Services'
-import ProductsHome from '../ProductsHome/ProductsHome'
 import ProductsContainer from '../ProductsHome/ProductsContainer'
 
 function Home() {
@@ -10,6 +10,7 @@ function Home() {
     <div>
 
       <img src={hero} alt="cortina" className='heroHomeImg' />
+      <img src={heroPc} alt="cortina" className='heroHomeImgDesktop' />
       
       <Services/>
       <ProductsContainer/>
