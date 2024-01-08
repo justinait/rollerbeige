27c510af0be929ed394d2a27aa139c639afafc7d
justinait <justiiturraspe@gmail.com>
2024-01-08 17:15:41 -0300

    about details finished

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import './About.css'
-import van from '/camioneta.png'
 import image from '/images/curtains/heroabout.png'
-import image2 from '/images/curtains/cortinas (73).jpg'
+import image2 from '/images/curtains/cortinas (74).jpg'
 
 
 function About() {
   return (
     <div className='aboutContainer'>
       
-      <img src={image} alt="" />
+      <h2> ROLLERBEIGE</h2>
+      <img src={image} className='aboutImgMobile' alt="" />
+      <img src={image2} alt="" className='aboutImgDesktop' />
       
       <p className='aboutText'>
         <h3>Nuestra historia</h3>
@@ -42,30 +43,9 @@ function About() {
         atención personalizada, tanto en la compra como en la postventa.
         <br />
         <br />
-        </p>
-        <p className='aboutText finalAbout'>Gracias por ser parte de nuestra historia.</p>
+      </p>
+      <p className='aboutText finalAbout'>Gracias por ser parte de nuestra historia.</p>
         
-{/* <p>
-  Bienvenidos a Roller Beige, un proyecto que nació en el corazón de una familia apasionada
-  por la creación de ambientes acogedores y estilos propios. Fundada a principios del año 2021,
-  nos hemos dedicado con esmero a la fabricación y venta de cortinas e insumos,
-  convirtiéndonos en una referencia en el mercado.
-  Para nosotros, la familia es nuestro pilar fundamental. Creemos en el esfuerzo conjunto como
-  herramienta de crecimiento y prosperidad, valores que se reflejan en cada detalle de nuestros
-  productos y de nuestros servicios. Somos más que una empresa, somos un equipo
-  comprometido con la excelencia y la satisfacción de nuestros clientes.
-  Nuestra experiencia y profesionalismo en el mundo de las cortinas nos permiten ofrecer un
-  asesoramiento único. Queremos compartir contigo nuestros conocimientos para que tomes la
-  mejor decisión al elegir tus cortinas, garantizando no solo un elemento decorativo, sino también
-  funcional para tu espacio.
-  En Roller Beige, cada cortina es más que un producto; es el resultado de nuestra dedicación y
-  amor por lo que hacemos. Estamos aquí para transformar tus espacios y hacer que cada rincón
-  de tu hogar o negocio refleje tu estilo único.
-  Gracias por ser parte de nuestra historia. Descubre el mundo de las cortinas con Roller Beige,
-  donde &quot;tenemos conciencia de servicio&quot; para ofrecerte una atención personalizada, tanto en la
-  compra como en la postventa.
-</p> */}
-
     </div>
   )
 }
