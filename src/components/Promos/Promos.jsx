cfd26e85e6eecde39965c506185c394be7c8618d
justinait <justiiturraspe@gmail.com>
2024-01-11 14:40:31 -0300

    modal

--- src/components/Promos/Promos.jsx
+++ src/components/Promos/Promos.jsx
@@ -1,13 +1,26 @@
 import React, { useEffect, useState } from 'react'
 import { db } from '../../firebaseConfig'
 import {getDocs, collection} from "firebase/firestore"
-import { Link } from 'react-router-dom';
 import './Promos.css'
 import promo1 from '/images/curtains/cortinas (3).jpg'
 import promo2 from '/images/curtains/cortinas (2).jpg'
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 function Promos() {
   const [products, setProducts] = useState([]);
+  const [show, setShow] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  
+  const handleClick =(e)=>{
+    if (!show) {
+      setSelectedItem(e);
+      handleShow();
+    }
+  }
 
   useEffect(()=>{
     let refCollection = collection(db, 'products')
@@ -30,7 +43,7 @@ function Promos() {
         {
           products.map(e=>{
             return(
-              <div key={e.id}  className='promoContainer'>
+              <div key={e.id}  className='promoContainer' onClick={()=>handleClick(e)}>
                 
                 <img src={e.image} alt={e.title} className='promoImage' />                
                 <p className='promoTitle'>{e.title}</p>
@@ -40,6 +53,20 @@ function Promos() {
             )
           })
         }
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>{selectedItem?.title} </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     </div>
   )
