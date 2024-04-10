b544bd8e77e54994d5f75a7c1ab0b6719d7eae21
justinait <justiiturraspe@gmail.com>
2024-04-10 16:32:57 -0300

    home store

--- src/components/HomeCart/HomeCart.jsx
+++ src/components/HomeCart/HomeCart.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import './HomeCart.css'
 import { collection, getDocs, limit, orderBy, query } from 'firebase/firestore';
 import { db } from '../../firebaseConfig';
+import { Link } from 'react-router-dom';
 
 function HomeCart() {
 
@@ -9,19 +10,19 @@ function HomeCart() {
     
     useEffect(()=>{
 
-        let refCollection = collection(db, 'products')
-        let q = query(refCollection, orderBy('unit_price'), limit(4));
-        getDocs(q)
-        .then((res)=>{
-          let newArray = res.docs.map(e =>{
-            return {
-              ...e.data(), 
-              id: e.id
-            }
-          })
-          setProducts(newArray);
+      let refCollection = collection(db, 'products')
+      let q = query(refCollection, orderBy('unit_price'), limit(4));
+      getDocs(q)
+      .then((res)=>{
+        let newArray = res.docs.map(e =>{
+          return {
+            ...e.data(), 
+            id: e.id
+          }
         })
-        .catch((err)=>console.log(err))
+        setProducts(newArray);
+      })
+      .catch((err)=>console.log(err))
     }, [])
     const numToShow = window.innerWidth < 1000 ? 2 : 4;
 
@@ -29,19 +30,19 @@ function HomeCart() {
     <div>
         <h3>NUESTRA TIENDA</h3>
         <div className='menuStore'>
-            {
-            products
-            .slice(0, numToShow)
-            .map((e, i)=>{
-                return(
-                <div key={e.id}  className='promoContainer' onClick={()=>handleClick(e)} style={{ cursor: 'pointer' }}>
-                    <img src={e.image} alt={e.title} className='promoImage' />                
-                    <p className='promoTitle'>{e.title}</p>
-                    <p className='promoPrice'>$ {e.unit_price}</p>
-                </div>
-                )
-            })
-            }
+          {
+          products
+          .slice(0, numToShow)
+          .map((e, i)=>{
+              return(
+              <Link to={'/store'} key={e.id}  className='promoContainer' style={{ cursor: 'pointer' }}>
+                <img src={e.image} alt={e.title} className='promoImage' />                
+                <p className='promoTitle'>{e.title}</p>
+                <p className='promoPrice'>$ {e.unit_price}</p>
+              </Link>
+              )
+          })
+          }
         </div>
     </div>
   )
