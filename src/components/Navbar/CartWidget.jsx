c9f25d92915da6e885a09f4da00fd6ecd51e17cc
justinait <justiiturraspe@gmail.com>
2024-04-10 17:14:50 -0300

    cart widget

--- src/components/Navbar/CartWidget.jsx
+++ src/components/Navbar/CartWidget.jsx
@@ -19,21 +19,25 @@ function CartWidget() {
     <div>
         {
             <div className='cartWidgetBox'>
-            {
-                cart.map((e, i)=>{
-                    return (
-                        <div key={i} className='cartWidgetItemContainer'>
-    
-                            <img className='cartWidgetImage' src={e.image} alt={e.title} />
-                            <div className='cartWidgetContainerInfo'>
-                                <h6>{e.title}</h6>
-                                <h6>{e.color}</h6>
-                                <p className='cartItemPrice'> {e.quantity} x ${e.unit_price}</p>
-                            </div>
-                        </div>
-                    )
-                })
-            }
+                {
+                    (cart.length == 0) ?
+                    <p className='cartWidgetEmpty'>Tu carrito esta vacío</p>:
+                    <div>
+                        {cart.map((e, i)=>{
+                            return (
+                                <div key={i} className='cartWidgetItemContainer'>
+            
+                                    <img className='cartWidgetImage' src={e.image} alt={e.title} />
+                                    <div className='cartWidgetContainerInfo'>
+                                        <h6>{e.title}</h6>
+                                        <h6>{e.color}</h6>
+                                        <p className='cartItemPrice'> {e.quantity} x ${e.unit_price}</p>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                }
             <p className='subtotalCart'>Subtotal: <strong>${total}</strong> </p>
             <Link className='cartWidgetButton' to='/cart' >Ver carrito</Link>
             </div>
