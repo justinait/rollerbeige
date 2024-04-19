a443fdeafb05af37fbc8e6d4cdaa5b01ce8559da
justinait <justiiturraspe@gmail.com>
2024-04-19 15:22:49 -0300

    spinner

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -4,12 +4,14 @@ import Whatsapp from '../Whatsapp/Whatsapp';
 import { ProductsContext } from '../../context/ProductsProvider';
 import { Link, useParams } from 'react-router-dom';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { Spinner } from 'react-bootstrap';
 
 function Detail() { 
   const { dataCurtains, dataFabrics } = useContext(ProductsContext);
   const param = useParams();
   const [product, setProduct] = useState([]);
   const [isFabric, setIsFabric] = useState(false);
+  const [loading, setLoading] = useState(true)
 
   const getProduct = () => {
     let foundProduct = null;
@@ -32,6 +34,7 @@ function Detail() {
         setTimeout(loadData, 100);//0.1segundo
         return;
       }
+      setLoading(false); 
       getProduct();
     };
     loadData();
@@ -45,44 +48,55 @@ function Detail() {
   }
 
   return (
-    <div className='detailContainer'>
-      {isFabric ?      
-      <Link to='/telas' className='arrow'>      <ArrowBackIcon/> Telas      </Link>  :
-      <Link to='/cortinas' className='arrow'>      <ArrowBackIcon/> Cortinas      </Link> 
-      }
-
-      <h3>{name}</h3>
-      <div className='ppalInfo'>
-        <img src={image} alt={name} className='detailImage'/>
-        <p dangerouslySetInnerHTML={{ __html: description }} className='detailDescription'></p>
+    <div>
+    {loading ? 
+      <div className='spinner'>
+        <Spinner animation="border" role="status" >
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
       </div>
+      : 
+      
+      <div className='detailContainer'>
+        {isFabric ?      
+        <Link to='/telas' className='arrow'>      <ArrowBackIcon/> Telas      </Link>  :
+        <Link to='/cortinas' className='arrow'>      <ArrowBackIcon/> Cortinas      </Link> 
+        }
 
-      <div className='detailsListContainer'>
-        
-        {Array.isArray(objectData) && objectData.map((e, i) => {
-          const values = attributes[e];
+        <h3>{name}</h3>
+        <div className='ppalInfo'>
+          <img src={image} alt={name} className='detailImage'/>
+          <p dangerouslySetInnerHTML={{ __html: description }} className='detailDescription'></p>
+        </div>
 
-          return (
-            <div key={i} className='detailsListItem'>
-              <h4 className='detailAttributeSubtitle'>{e}</h4>
-              {Array.isArray(values) ? (
-                <ul>
-                  {values.map((item, k) => (
-                    <li key={k}>{item}</li>
-                  ))}
-                </ul>
-              ) : (
-                <ul>
-                  <li>{values}</li>
-                </ul>
-              )}
-            </div>
-          );
-        })}
+        <div className='detailsListContainer'>
+          
+          {Array.isArray(objectData) && objectData.map((e, i) => {
+            const values = attributes[e];
+
+            return (
+              <div key={i} className='detailsListItem'>
+                <h4 className='detailAttributeSubtitle'>{e}</h4>
+                {Array.isArray(values) ? (
+                  <ul>
+                    {values.map((item, k) => (
+                      <li key={k}>{item}</li>
+                    ))}
+                  </ul>
+                ) : (
+                  <ul>
+                    <li>{values}</li>
+                  </ul>
+                )}
+              </div>
+            );
+          })}
+        </div>
+        {isFabric ?      
+        <Link to='/telas' className='arrow'>      <ArrowBackIcon/> Telas      </Link>  :
+        <Link to='/cortinas' className='arrow'>      <ArrowBackIcon/> Cortinas      </Link> 
+        }
       </div>
-      {isFabric ?      
-      <Link to='/telas' className='arrow'>      <ArrowBackIcon/> Telas      </Link>  :
-      <Link to='/cortinas' className='arrow'>      <ArrowBackIcon/> Cortinas      </Link> 
       }
     </div>
   );
