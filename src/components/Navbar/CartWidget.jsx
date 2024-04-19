ef568159fd50e0088efc7f5a3dd56a40dbc8199d
justinait <justiiturraspe@gmail.com>
2024-04-19 15:57:19 -0300

    details

--- src/components/Navbar/CartWidget.jsx
+++ src/components/Navbar/CartWidget.jsx
@@ -15,35 +15,33 @@ function CartWidget() {
 
     let total = getTotalPrice()
 
-  return (
-    <div>
-        {
-            <div className='cartWidgetBox'>
-                {
-                    (cart.length == 0) ?
-                    <p className='cartWidgetEmpty'>Tu carrito esta vacío</p>:
-                    <div className='cartWidgetItemsContainer'>
-                        {cart.map((e, i)=>{
-                            return (
-                                <div key={i} className='cartWidgetItemContainer'>
-            
-                                    <img className='cartWidgetImage' src={e.image} alt={e.title} />
-                                    <div className='cartWidgetContainerInfo'>
-                                        <h6>{e.title}</h6>
-                                        <h6>{e.color}</h6>
-                                        <p className='cartItemPrice'> {e.quantity} x ${e.unit_price}</p>
-                                    </div>
-                                </div>
-                            )
-                        })}
-                    </div>
-                }
-                <p className='subtotalCart'>Subtotal: <strong>${total}</strong> </p>
-                <Link className='cartWidgetButton' to='/cart' >Ver carrito</Link>
-            </div>
-        }
+  
+    return (
+
+        <div className='cartWidgetBox'>
+            {
+                (cart.length == 0) ?
+                <p className='cartWidgetEmpty'>Tu carrito esta vacío</p>:
+                <div className='cartWidgetItemsContainer'>
+                    {cart.map((e, i)=>{
+                        return (
+                            <div key={i} className='cartWidgetItemContainer'>
         
-    </div>
+                                <img className='cartWidgetImage' src={e.image} alt={e.title} />
+                                <div className='cartWidgetContainerInfo'>
+                                    <h6>{e.title}</h6>
+                                    <h6>{e.color}</h6>
+                                    <p className='cartItemPrice'> {e.quantity} x ${e.unit_price}</p>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            }
+            <p className='subtotalCart'>Subtotal: <strong>${total}</strong> </p>
+            <Link className='cartWidgetButton' to='/cart' >Ver carrito</Link>
+        </div>
+        
   )
 }
 
